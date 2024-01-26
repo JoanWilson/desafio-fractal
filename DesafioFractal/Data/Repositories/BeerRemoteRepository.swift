dc6172beeb8c57528da8f11eda0cd638daae55aa
Joan Wilson <joanwmoliveira@gmail.com>
2024-01-26 04:11:43 -0300

    feat: create repository implementation class

---
+++ DesafioFractal/Data/Repositories/BeerRemoteRepository.swift
@@ -0,0 +1,23 @@
+//
+//  BeerRemoteRepository.swift
+//  DesafioFractal
+//
+//  Created by Joan Wilson Oliveira on 26/01/24.
+//
+
+import Foundation
+
+final class BeerRemoteRepository: BeerRemoteRepositoryProtocol {
+    
+    func listBeers(page: Int, amount: Int?) -> [Beer] {
+        return []
+    }
+    
+    func searchBeer(byName inputText: String) -> [Beer] {
+        return []
+    }
+    
+    func requestBeer(completion: @escaping (Result<Beer, Error>) -> Void) {
+        
+    }
+}
