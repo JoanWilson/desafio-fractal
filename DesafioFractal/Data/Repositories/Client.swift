f02b7726dd4dd319bc689a8885d5b44ed60834f3
Joan Wilson <joanwmoliveira@gmail.com>
2024-01-26 18:54:40 -0300

    feat: implement getAllBeersUseCases

--- DesafioFractal/Data/Repositories/Client.swift
+++ DesafioFractal/Data/Repositories/Client.swift
@@ -11,7 +11,7 @@ enum ErrorClient: Error {
     case invalidBaseURL
 }
 
-final class PunkApiClient {
+final class PunkApiClient: BeerRemoteRepository {
     
     private let apiURL: String = "https://api.punkapi.com/v2/beers?page=1"
     private let session: URLSession = .init(configuration: .default)
